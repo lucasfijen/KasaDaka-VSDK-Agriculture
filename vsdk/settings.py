ba59f8bf977459f0d3725dc3dd8188792812cf82
Lucas <lucas.fijen@gmail.com>
2019-04-30 15:52:24 +0200

    removed our polls app

--- vsdk/settings.py
+++ vsdk/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'tk2(l(00&kfe7j97j$dvgz&b6r!kk_zbse1(9w*eoc$bcwu773'
 ##########
 #Use True on your local PC, False on Heroku!!
 ########
-#DEBUG = True
+# DEBUG = True
 
 DEBUG = False
 
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
 ]
 
 
--- vsdk/urls.py
+++ vsdk/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
-from django.urls import path
 
 admin.site.site_header = _("KasaDaka Voice Services")
 
@@ -26,8 +25,7 @@ urlpatterns = [
     url(r'^', admin.site.urls),
     url(r'^vxml/', include('vsdk.service_development.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns.insert(0, path('polls/', include('polls.urls')))
-urlpatterns.insert(1, path('admin/', admin.site.urls))
+
 #if not settings.DEBUG:
 #        urlpatterns += urlpatterns('',
 #                (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True, }),
