a97d06961bcb91fb241045cf43d6469065062c09
Lucas <lucas.fijen@gmail.com>
2019-05-15 13:23:17 +0200

    start on show_offers

--- vsdk/service_development/views/offer.py
+++ vsdk/service_development/views/offer.py
@@ -19,15 +19,29 @@ def create_requests(session):
                 'questions': [question.get_voice_fragment_url(language) for question in questions],
                 'voice_labels': [offer.voice_label.get_voice_fragment_url(language) for offer in offers],
                 'language': language,
-                'choice_options_redirect_urls': ['vxml/offer_redirect/' + str(offer.id)  for offer in offers]
+                'choice_options_redirect_urls': ['vxml/show_offer/' str(session.id) + '/' + str(offer.id)  for offer in offers]
                 }
     return context
 
-def offer(request, element_id, session_id):
+def offer(request, session_id):
 
     session = get_object_or_404(CallSession, pk=session_id)
-    # session.record_step(offer_element)
+    session.record_step(None, 'Showed the offers')
     
     context = create_requests(session)
     
     return render(request, 'offer.xml', context, content_type='text/xml')
+
+
+def show_offer(request, session_id, offer_id):
+
+    session = get_object_or_404(CallSession, pk=session_id)
+    session.record_step(None, 'Showing offer %s' % offer_id)
+    offer = get_object_or_404(Offer, pk=offer_id)
+
+    context  = {'pre_offer_voice': get_object_or_404(VoiceLabel, name='pre_offer_voice').get_voice_gragment_url(language),
+                'number': ['listofnumbers'],
+                'post_offer_voice': get_object_or_404(VoiceLabel, name='post_offer_voice').get_voice_gragment_url(language),
+                }
+
+    return render(request, 'show_offer.xml', context, content_type='text/xml')
