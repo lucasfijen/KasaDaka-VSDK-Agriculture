26f5f6aa793ddde412ba80bf0aae820374bc225b
Lucas <lucas.fijen@gmail.com>
2019-05-22 21:01:31 +0200

    Last changes, lets try!

--- vsdk/service_development/views/lendrentredirect.py
+++ vsdk/service_development/views/lendrentredirect.py
@@ -4,8 +4,10 @@ from django.shortcuts import *
 
 def lendrentredirect(request, session_id):
     session = get_object_or_404(CallSession, pk=session_id)
-
+    vse_element = get_object_or_404(Vse_Own_Added, name='lendorrentredirect')
     if session._lending:
+        # Goes to offers, where user will hear offers
         return HttpResponseRedirect(reverse( 'service-development:offers', kwargs= {'session_id':session.id}))
     else:
-        return HttpResponseRedirect(reverse( 'service-development:offers', kwargs= {'session_id':session.id}))
+        # Goes to place to record audio and place the offer
+        return HttpResponseRedirect(vse_element.redirect.get_absolute_url(session=session))
--- vsdk/service_development/views/offerplacing.py
+++ vsdk/service_development/views/offerplacing.py
@@ -3,4 +3,18 @@ from django.shortcuts import *
 
 def offerplacing(request, session_id):
     session = get_object_or_404(CallSession, pk=session_id)
-    # Offer(models.Model)
+    newoffer = Offer(product_type = session._product,
+                     region = session._region,
+                     user_id = session.user,
+                     language = session.language)
+    try:
+        recording = get_object_or_404(SpokenUserInput, session = session)
+        newoffer.audio_file = recording
+    except:
+        print('audiorecording went wrong')
+        pass
+    newoffer.create_enddate()
+    newoffer.save()
+    final_audio_url = get_object_or_404(VoiceLabel, name='save_recording').get_voice_fragment_url(session.language)
+    context = {'final_message': final_audio_url}
+    render(request, 'product_selection.xml', context, content_type='text/xml')
